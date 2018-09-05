804a275df5ccbbc39889a043627407e1d827788a
travis <travis>
2018-09-05 10:58:35 +0000

    Automatic update for 48ed83cfcbbcbc62cd0a82a2fd2c7f2ea2f64623

---
+++ implementors/core/ops/bit/trait.Shl.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["ipnet"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Shl.html\" title=\"trait core::ops::bit::Shl\">Shl</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>&gt; for <a class=\"struct\" href=\"ipnet/struct.Emu128.html\" title=\"struct ipnet::Emu128\">Emu128</a>",synthetic:false,types:["ipnet::emu128::Emu128"]},];
+implementors["openssl"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Shl.html\" title=\"trait core::ops::bit::Shl\">Shl</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i32.html\">i32</a>&gt; for &amp;'a <a class=\"struct\" href=\"openssl/bn/struct.BigNumRef.html\" title=\"struct openssl::bn::BigNumRef\">BigNumRef</a>",synthetic:false,types:["openssl::bn::BigNumRef"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.Shl.html\" title=\"trait core::ops::bit::Shl\">Shl</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i32.html\">i32</a>&gt; for &amp;'a <a class=\"struct\" href=\"openssl/bn/struct.BigNum.html\" title=\"struct openssl::bn::BigNum\">BigNum</a>",synthetic:false,types:["openssl::bn::BigNum"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
